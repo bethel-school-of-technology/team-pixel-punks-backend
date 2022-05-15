f3bd63730323de284c49ee38aaffb12f8a497467
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-05-15 12:19:06 -0600

    JWT & Bcrypt done
    should be functioning.  The redirect to the 'profile' page is not quite working, but will need to be changed when integrating back and front ends.

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,20 @@ const Connection = require('mysql/lib/Connection');
 var router = express.Router();
 const models = require('../models');
 var passport = require('../services/passport');
+var authService = require('../services/auth');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+//render the signup page when navigating there
 router.get('/signup', function (req, res, next) {
   res.render('signup');
 });
 
+//take in form for signing up for a new user and redirect
+//to the login page
 router.post('/signup', function (req, res, next) {
   models.users
     .findOrCreate({
@@ -23,7 +27,7 @@ router.post('/signup', function (req, res, next) {
         FirstName: req.body.firstName,
         LastName: req.body.lastName,
         Email: req.body.email,
-        Password: req.body.password
+        Password: authService.hashPassword(req.body.password)
       }
     })
     .spread(function (result, created) {
@@ -35,36 +39,62 @@ router.post('/signup', function (req, res, next) {
     });
 });
 
+//render the login page when navigating to this route
 router.get('/login', function (req, res, next) {
   res.render('login');
 });
 
-router.post('/login', passport.authenticate('local', {
-  failureRedirect: '/users/login'
-}),
-  function (req, res, next) {
-    res.redirect('profile');
+//take in username and password, authenticate the user or give them the boot
+router.post('/login', function (req, res, next) {
+  models.users.findOne({
+    where: {
+      Username: req.body.username
+    }
+  }).then(user => {
+    if (!user) {
+      console.log('User not found')
+      return res.status(401).json({
+        message: "Login Failed"
+      });
+    } else {
+      let passwordMatch = authService.comparePasswords(req.body.password, user.Password);
+      if (passwordMatch) {
+        let token = authService.signUser(user);
+        res.cookie('jwt', token);
+        res.redirect('profile');
+      } else {
+        console.log('Wrong password');
+        res.send('Wrong password');
+      }
+    }
   });
+});
 
+//profile route will need to be changed to /locations when connecting
+//to the front end, i used the one from the lesson to get it working
+//for now.
 router.get('/profile', function (req, res, next) {
-  if (req.user) {
-    models.users
-      .findByPk(parseInt(req.user.UserId))
+  let token = req.cookies.jwt;
+  if (token) {
+    authService.verifyUser(token)
       .then(user => {
         if (user) {
-          res.render('profile', {
-            FirstName: user.FirstName,
-            LastName: user.LastName,
-            Email: user.Email,
-            Username: user.Username
-          });
+          res.send(JSON.stringify(user));
         } else {
-          res.send('User not found');
+          res.status(401);
+          res.send('Invalid authentication token');
         }
       });
   } else {
-    res.redirect('/users/login');
+    res.status(401);
+    res.send('Must be logged in');
   }
 });
+
+//route for logging the user out, and clearing the jwt token
+router.get('/logout', function (req, res, next) {
+  res.cookie('jwt', "", { expires: new Date(0) });
+  res.send('Logged out');
+  });
 
 module.exports = router;
---
+++ services/auth.js
@@ -0,0 +1,48 @@
+const jwt = require('jsonwebtoken');
+const models = require('../models/index');
+const bcrypt = require("bcryptjs");
+
+//define the object that does the authentication service
+var authService = {
+
+  //creates a jwt token for the user to use, allows them access
+  signUser: function(user) {
+    const token = jwt.sign(
+      {
+        Username: user.Username,
+        UserId: user.UserId
+      },
+      'secretkey',
+      {
+        expiresIn: '1h'
+      }
+    );
+    return token;
+  },
+
+  //verify the user is authorized
+  verifyUser: function (token) {  //<--- receive JWT token as parameter
+    try {
+      let decoded = jwt.verify(token, 'secretkey'); //<--- Decrypt token using same key used to encrypt
+      return models.users.findByPk(decoded.UserId); //<--- Return result of database query as promise
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  },
+
+  //encrypt the password
+  hashPassword: function(plainTextPassword) {
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(plainTextPassword, salt);
+    return hash;
+  },
+  //rehash entered password and compare it to the one from the 
+  //database
+  comparePasswords: function (plainTextPassword, hashedPassword) {
+    return bcrypt.compareSync(plainTextPassword, hashedPassword)
+  }
+  
+}
+
+module.exports = authService;
