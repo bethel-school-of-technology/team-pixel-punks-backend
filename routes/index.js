92414ee0dd8cddebb731c6c59b0040f7019161e3
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-05-14 19:42:05 -0600

    added post route for sign-up page
    Back end ability to create a new user account

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
+const { registerAsyncHelper } = require('hbs');
 var router = express.Router();
 var mysql = require('mysql2');
+const models = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+//from the create an account page, take in the data from the form
+//create row in database
+router.post('/sign-up', function (req, res, next) {
+  models.users.create(req.body)
+    .then(newUser => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(newUser));
+    })
+    .catch(err => {
+      res.status(400);
+      res.send(err.message);
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 var express = require('express');
+const Connection = require('mysql/lib/Connection');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
